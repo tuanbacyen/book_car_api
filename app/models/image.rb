65a07b43a4afb7cc2c17c523b0034592efe71c4d
Tuan Pham <tuanbacyen@gmail.com>
2018-10-10 13:23:33 +0700

    destroy image from cloudinary

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,11 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :car
+  before_destroy :delete_img
+
+  private
+
+  def delete_img
+    Cloudinary::Uploader.destroy(image.file.public_id)
+  end
 end
